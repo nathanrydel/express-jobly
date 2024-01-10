1e5e6cd7b47060664fa6ef589a08f1b9f8f5fd3a
Nathan Rydel <nathanrydel@gmail.com>
2024-01-10 12:45:32 -0700

    added docstring for sqlForPartialUpdate function

--- helpers/sql.js
+++ helpers/sql.js
@@ -3,6 +3,23 @@
 const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Helper function to make updates to database based on variatic input
+ *
+ * @param {Object} dataToUpdate - the data that you want to edit
+ * @param {Object} jsToSql - A JS syntax key with the database col as a value
+ * @returns {Object} -
+ *  {
+ *   setCols: the sql needed to update a col,
+ *   values: array of values used in sanitize statement
+ * }
+ *
+ * @example {firstName: 'Aliya', age: 32} =>
+ *  {
+ *    setCols: '"first_name"=$1', '"age"=$2'
+ *    values: ['Aliya', 32]
+ * }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -10,7 +27,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
