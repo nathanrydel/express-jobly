dbdc304a90dddb5a93220c2630e7f4ed2a700352
Nathan Rydel <nathanrydel@gmail.com>
2024-01-10 12:48:13 -0700

    added test for more than one change

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -3,7 +3,7 @@
 const { sqlForPartialUpdate } = require("./sql");
 
 describe("sqlForPartialUpdate tests", function () {
-  test("working correctly", function () {
+  test("single change", function () {
     const result = sqlForPartialUpdate(
       { firstName: "edited_value" },
       { firstName: "first_name", lastName: "last_name" }
@@ -13,8 +13,18 @@ describe("sqlForPartialUpdate tests", function () {
       setCols: "\"first_name\"=$1",
       values: ["edited_value"]
     });
-  }
+  });
 
-  );
+  test("more than one change", function () {
+    const result = sqlForPartialUpdate(
+      { firstName: "edit_fname", lastName: "edited_lname" },
+      { firstName: "first_name", lastName: "last_name" }
+    );
+
+    expect(result).toEqual({
+      setCols: "\"first_name\"=$1, \"last_name\"=$2",
+      values: ["edit_fname", "edited_lname"]
+    });
+  });
 
 });
