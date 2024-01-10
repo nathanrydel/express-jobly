ff7067d75d6a6ff8087870edf7258afef7bf2c75
Nathan Rydel <nathanrydel@gmail.com>
2024-01-10 15:13:43 -0700

    updating testing for 100% coverage of sql.js

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -6,7 +6,7 @@ describe("sqlForPartialUpdate tests", function () {
   test("single change", function () {
     const result = sqlForPartialUpdate(
       { firstName: "edited_value" },
-      { firstName: "first_name", lastName: "last_name" }
+      { firstName: "first_name" }
     );
 
     expect(result).toEqual({
@@ -17,12 +17,12 @@ describe("sqlForPartialUpdate tests", function () {
 
   test("more than one change", function () {
     const result = sqlForPartialUpdate(
-      { firstName: "edit_fname", lastName: "edited_lname" },
+      { firstName: "edit_fname", l_name: "edited_lname" },
       { firstName: "first_name", lastName: "last_name" }
     );
 
     expect(result).toEqual({
-      setCols: "\"first_name\"=$1, \"last_name\"=$2",
+      setCols: "\"first_name\"=$1, \"l_name\"=$2",
       values: ["edit_fname", "edited_lname"]
     });
   });
