5b2ddc2d46afcca115d5657ed8197100f180ed2b
Nathan Rydel <nathanrydel@gmail.com>
2024-01-11 19:01:36 -0700

    added tests for findAll method and get method

--- models/job.test.js
+++ models/job.test.js
@@ -18,15 +18,6 @@ afterAll(commonAfterAll);
 /************************************** create */
 
 
-// CREATE TABLE jobs (
-//   id SERIAL PRIMARY KEY,
-//   title TEXT NOT NULL,
-//   salary INTEGER CHECK (salary >= 0),
-//   equity NUMERIC CHECK (equity <= 1.0),
-//   company_handle VARCHAR(25) NOT NULL
-//     REFERENCES companies ON DELETE CASCADE
-// );
-
 describe("create", function () {
   const newJob = {
     title: "job",
@@ -69,27 +60,77 @@ describe("create", function () {
         salary: 1000,
         equity: "0.5",
         company_handle: "error",
-      }
+      };
       expect(async () =>
         await Job.create(invalidJob))
         .toThrow(BadRequestError);
     });
 });
 
+/************************************** findAll */
 
+describe("findAll", function () {
+  test("works: no filter", async function () {
+    let jobs = await Job.findAll();
 
+    expect(jobs).toEqual([
+      {
+        id: expect.any(Number),
+        title: "test_job_1",
+        salary: 1000,
+        equity: "0.5",
+        company_handle: "c1",
+      },
+      {
+        id: expect.any(Number),
+        title: "test_job_2",
+        salary: 9001,
+        equity: "0.1",
+        company_handle: "c2",
+      },
+      {
+        id: expect.any(Number),
+        title: "test_job_3",
+        salary: 1499,
+        equity: "0.3",
+        company_handle: "c3",
+      },
+    ]);
+  });
+});
 
-
-
-
-
-
+/************************************** findFilteredJobs */
+// TODO:
 
 /************************************** get */
 
+// CREATE TABLE jobs (
+//   id SERIAL PRIMARY KEY,
+//   title TEXT NOT NULL,
+//   salary INTEGER CHECK (salary >= 0),
+//   equity NUMERIC CHECK (equity <= 1.0),
+//   company_handle VARCHAR(25) NOT NULL
+//     REFERENCES companies ON DELETE CASCADE
+// );
 
+describe("get", function () {
+  test("works", async function () {
+    const job = await Job.get(1);
 
+    expect(job).toEqual({
+      id: 1,
+      title: "test_job_1",
+      salary: 1000,
+      equity: "0.5",
+      company_handle: "c1",
+    });
+  });
 
+  test("not found if no such job", async function () {
+    expect(async () => await Job.get("invalidJob"))
+      .toThrow(NotFoundError);
+  });
+});
 
 
 
