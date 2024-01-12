0c8a1230556a12d30702f13a4ec306ab54c7525e
Nathan Rydel <nathanrydel@gmail.com>
2024-01-12 13:10:23 -0700

    overhaul of common testing practices

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -5,15 +5,16 @@ const bcrypt = require("bcrypt");
 const db = require("../db.js");
 const { BCRYPT_WORK_FACTOR } = require("../config");
 
+const testJobIds = [];
+
 async function commonBeforeAll() {
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM companies");
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM users");
-
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM jobs");
-
+  // reset jobs sequence to start at 1
   await db.query("ALTER SEQUENCE jobs_id_seq RESTART WITH 1");
 
   await db.query(`
@@ -21,6 +22,15 @@ async function commonBeforeAll() {
       VALUES ('c1', 'C1', 1, 'Desc1', 'http://c1.img'),
              ('c2', 'C2', 2, 'Desc2', 'http://c2.img'),
              ('c3', 'C3', 3, 'Desc3', 'http://c3.img')`);
+
+  const resultsJobs = await db.query(`
+    INSERT INTO jobs (title, salary, equity, company_handle)
+    VALUES ('Job1', 100, '0.1', 'c1'),
+           ('Job2', 200, '0.2', 'c1'),
+           ('Job3', 300, '0', 'c1'),
+           ('Job4', NULL, NULL, 'c1')
+    RETURNING id`);
+  testJobIds.splice(0, 0, ...resultsJobs.rows.map(r => r.id));
 
   await db.query(`
       INSERT INTO users(username,
@@ -36,10 +46,9 @@ async function commonBeforeAll() {
   ]);
 
   await db.query(`
-      INSERT INTO jobs(title, salary, equity, company_handle)
-      VALUES ('j1', 100, '0.1', 'c1'),
-             ('j2', 200, '0.2', 'c2'),
-             ('j3', 300, '0.3', 'c3')`);
+        INSERT INTO applications(username, job_id)
+        VALUES ('u1', $1)`,
+    [testJobIds[0]]);
 }
 
 async function commonBeforeEach() {
@@ -60,4 +69,5 @@ module.exports = {
   commonBeforeEach,
   commonAfterEach,
   commonAfterAll,
+  testJobIds,
 };
